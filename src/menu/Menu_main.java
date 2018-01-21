2da9d6a38c69c3078288dae55b6b38d7a6f8312d
50616017 <50616017@192.168.11.3>
2018-01-21 10:12:31 +0900

    Menu

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,14 @@
+package menu;
+
+import menu.Menu_view;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view frame = new Menu_view();
+        frame.setBounds(5,5,755,555);
+        frame.setVisible(true);
+	}
+
+}
